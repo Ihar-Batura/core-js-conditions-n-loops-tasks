0df51d8ec1e4d16087d0c8e391f73e1dd073f657
Ihar-Batura <A17331582@gmail.com>
2023-12-22 11:36:55 +0300

    feat: add code to isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -204,8 +204,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const dig = String(digit);
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === dig) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
